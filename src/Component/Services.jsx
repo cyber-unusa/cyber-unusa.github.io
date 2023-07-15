cc11363f12a4d3186485d3b61da0554d879f1663
wava43s <3130022043@student.unusa.ac.id>
2023-07-15 23:09:58 +0700

    Pembaruan Service

--- src/Component/Services.jsx
+++ src/Component/Services.jsx
@@ -1,16 +1,16 @@
 import Footer from "../UI/Footer";
 import { Navbar } from "../UI/Navbar"
-import {  money,bag,task } from "../assets/img/Icons";
+import {  money,bag,task, webDev, uiux, rakitPc,undangan,fastWorking,codesBracket,rocket,icons1 } from "../assets/img/Icons";
 export const Services=()=>{
     return (
       <>
         <Navbar />
         <section id="head" className="">
-          <div className="container px-4 md:px-4 lg:px-16 pb-12 flex flex-col md:flex-row">
-            <div className="contens md:basis-1/2 lg:basis-2/4 pt-10 md:pt-20 lg:pt-36">
+          <div className="container px-4 md:px-8 lg:px-16 pb-12 flex flex-col md:flex-row">
+            <div className="contens md:w-1/2 lg:w-2/4 pt-10 md:pt-20 lg:pt-36">
               <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold text-primary">
                 PERLU
-                <span className="font-bold border-0 rounded-md bg-primary text-white">
+                <span className="font-bold border-0 rounded-md bg-primary text-white m-4">
                   <span className="px-4 py-6">WEBSITE</span>
                 </span>
               </h1>
@@ -27,7 +27,7 @@ export const Services=()=>{
                 CYBERINAJA
               </p>
             </div>
-            <div className="contens-img md:basis-1/2 lg:basis-2/4 md:mt-12 lg:mt-4">
+            <div className="contens-img md:w-1/2 lg:w-2/4 md:mt-12 lg:mt-4">
               <img
                 src="/asset/services-img.jpg"
                 alt=""
@@ -39,31 +39,199 @@ export const Services=()=>{
 
         <section id="whyChooseMe" className="bg-secbg">
           <div className="container text-center py-12">
-            <h1 className="text-xl lg:text-3xl font-bold text-primary  ">
+            <h1 className="text-xl lg:text-3xl font-bold text-primary">
               WHY CHOOSE ME?
             </h1>
-            <div className="itemsz flex items-center text-center pt-16">
-              <div className="basis-3/4 px-10">
+            <div className="itemsz flex flex-col md:flex-row text-center pt-16">
+              <div className="basis-3/4 px-4 md:px-10 lg:px-16 mb-8 md:mb-0">
                 <div className="m-auto w-2/4">{money()}</div>
-                <h1 className="mt-12">Paling Murah Dan Terjangkau !!! </h1>
+                <h1 className="mt-12">Paling Murah Dan Terjangkau !!!</h1>
               </div>
-              <div className="basis-3/4 px-10">
+              <div className="basis-3/4 px-4 md:px-10 lg:px-16 mb-8 md:mb-0">
                 <div className="m-auto w-2/4">{bag()}</div>
                 <h1 className="mt-12">Professional Team</h1>
               </div>
-              <div className="basis-3/4 px-10">
+              <div className="basis-3/4 px-4 md:px-10 lg:px-16">
                 <div className="m-auto w-2/4">{task()}</div>
                 <h1 className="mt-12">Terpercaya Dan Terstruktur</h1>
               </div>
             </div>
           </div>
         </section>
+
         <section id="WhatWeDo?">
-          <div className="container text-center py-12">
-            <h1 className="text-xl lg:text-3xl font-bold text-primary  ">
+          <div className="container text-center py-12 px-4">
+            <h1 className="text-xl lg:text-3xl font-bold text-primary">
               WHAT WE DO?
             </h1>
-            <div className="flex"></div>
+            <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-20">
+              <div
+                id="webDeveloper"
+                className="basis-3/4 shadow-lg mx-2 md:mx-4 lg:mx-8 mt-8"
+              >
+                <div className="images">
+                  <span>{webDev()}</span>
+                </div>
+                <div className="contens text-white bg-primary py-4 px-4 h-[300px] rounded-b-lg mt-4">
+                  <h2 className="font-extrabold text-2xl">Web Developer</h2>
+                  <p className="font-bold text-sm pt-4">
+                    Bikin bisnismu ataupun apapun yang kamu inginkan menjadi
+                    digital yuk
+                  </p>
+                </div>
+              </div>
+              <div
+                id="uiUx"
+                className="basis-3/4 shadow-lg mx-2 md:mx-4 lg:mx-8 mt-8"
+              >
+                <div className="images">
+                  <span>{uiux()}</span>
+                </div>
+                <div className="contens text-white bg-primary py-4 px-4 h-[300px] rounded-b-lg mt-4">
+                  <h2 className="font-extrabold text-2xl">UI/UX Desain</h2>
+                  <p className="font-bold text-sm pt-4">
+                    Perbagus tampilanmu yuk agar orang-orang betah
+                  </p>
+                </div>
+              </div>
+              <div
+                id="rakitPc"
+                className="basis-3/4 shadow-lg mx-2 md:mx-4 lg:mx-8 mt-8"
+              >
+                <div className="images">
+                  <span>{rakitPc()}</span>
+                </div>
+                <div className="contens text-white bg-primary py-4 px-4 h-[300px] rounded-b-lg mt-4">
+                  <h2 className="font-extrabold text-2xl">Rakit PC</h2>
+                  <p className="font-bold pt-4 text-sm">
+                    Bingung mau beli komputer dan laptop. Titip kami aja , nanti
+                    dicari kan yang bagus dan sesuai
+                  </p>
+                </div>
+              </div>
+              <div
+                id="webDeveloper"
+                className="basis-3/4 shadow-lg mx-2 md:mx-4 lg:mx-8 mt-8"
+              >
+                <div className="images">
+                  <span>{fastWorking()}</span>
+                </div>
+                <div className="contens text-white bg-primary py-4 px-4 h-[300px] rounded-b-lg mt-4">
+                  <h2 className="font-extrabold text-2xl">Instalasi Laptop</h2>
+                  <p className="font-bold pt-4 text-sm">
+                    Bingung mau instal ulang windows, aplikasi lain dan
+                    sebagainya. Serah kan kami aja. Gak sampai 1 hari !!
+                  </p>
+                </div>
+              </div>
+              <div
+                id="webDeveloper"
+                className="basis-3/4 shadow-lg mx-2 md:mx-4 lg:mx-8 mt-8"
+              >
+                <div className="images">
+                  <span>{undangan()}</span>
+                </div>
+                <div className="contens text-white bg-primary py-4 px-4 h-[300px] rounded-b-lg mt-4">
+                  <h2 className="font-extrabold text-2xl">Undangan Digital</h2>
+                  <p className="font-bold pt-4 text-sm">
+                    Gak perlu pusing bikin undangan lagi!!. Digitalin aja
+                    disini!!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="Konsultasi" className="bg-secbg">
+          <div className="container flex flex-col md:flex-row p-12">
+            <div className="item1 basis-2/4 md:basis-1/2 md:pt-0 lg:basis-1/2 relative pt-24">
+              <h1 className="text-primary font-extrabold text-3xl max-sm:text-xl md:text-2xl">
+                Kami sudah membantu beberapa
+              </h1>
+              <h1 className="text-primary font-extrabold text-3xl max-sm:text-xl md:text-2xl">
+                Orang untuk naik level. Kamu kapan?
+              </h1>
+              <h1 className="font-extrabold text-4xl max-sm:text-xl  bg-primary md:w-2/4 max-sm:w-2/4 lg:w-40 p-2  text-white rounded-md mt-4 md:text-2xl">
+                #DIGITALIN <span className="block">AJA</span>
+              </h1>
+              <span className="absolute z-10 lg:-top-24 lg:right-48 max-sm:-top-4 max-sm:left-[18rem] max-sm:w-32 md:-top-20 md:right-0">
+                {icons1()}
+              </span>
+              <div className="absolute z-10 lg:bottom-56 lg:right-36  max-sm:-bottom-24 max-sm:right-16 md:-right-4 md:bottom-72">
+                {rocket()}
+              </div>
+              <div className="absolute z-10 lg:bottom-32 lg:-left-8 max-sm:-bottom-36 max-sm:-left-8 md:bottom-52 md:-left-8">
+                {codesBracket()}
+              </div>
+            </div>
+            <div className="item2 basis-2/4 md:basis-1/2 lg:basis-1/2 max-sm:pt-44 md:ml-8">
+              <div className="headTitle flex">
+                <div className="contens1 max-sm:m-auto">
+                  <h1 className="text-primary font-extrabold text-3xl max-sm:text-2xl  md:text-xl">
+                    Yuk Konsultasi <span className="block">Sekarang Juga</span>
+                  </h1>
+                </div>
+                <div className="contens2 pl-4 items-center pt-1">
+                  <h1 className="text-white font-extrabold rounded-md text-4xl bg-primary p-3 md:text-2xl">
+                    GRATIS
+                  </h1>
+                </div>
+              </div>
+              <div className="formulir mt-16 max-sm:text-center max-sm:m-auto max-sm:mt-20">
+                <p className="text-center text-primary font-bold">
+                  Isi formulir dibawah ini agar kami bisa membantu anda
+                </p>
+                <form action="" className="pt-8">
+                  <div className="flex">
+                    <div id="nama" className="basis-2/4">
+                      <span className="block text-primary font-bold">Nama</span>
+                      <input
+                        type="text"
+                        className="bg-primary w-3/5 rounded-md"
+                      />
+                    </div>
+                    <div id="email" className="basis-2/4">
+                      <span className="block text-primary font-bold">
+                        Email
+                      </span>
+                      <input
+                        type="text"
+                        className="bg-primary w-3/5 rounded-md"
+                      />
+                    </div>
+                  </div>
+
+                  <div id="layanans" className="pt-4 ">
+                    <span className="block text-primary font-bold">
+                      Layanan Yang Diperlukan
+                    </span>
+                    <select
+                      name="layanan"
+                      className="bg-primary p-2 mt-2 text-white font-bold rounded-md w-4/5"
+                    >
+                      <option value="">Web Development</option>
+                      <option value="">UI/UX Desain</option>
+                      <option value="">Rakit PC</option>
+                      <option value="">Instalasi Komputer</option>
+                      <option value="">Undangan Digital</option>
+                    </select>
+                  </div>
+                  <div id="kendalas" className="pt-4">
+                    <span className="block text-primary font-bold">
+                      Kendala Yang Dihadapi
+                    </span>
+                    <textarea
+                      rows="6"
+                      className="bg-primary mt-2 w-4/5 rounded-md p-3 text-white resize-none"
+                    ></textarea>
+                    <button className="bg-yellow p-2 block rounded-md mt-4 text-primary font-bold max-sm:m-auto max-sm:mt-4">
+                      SUBMIT
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
         </section>
         <Footer />
