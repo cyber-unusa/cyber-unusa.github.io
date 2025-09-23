22cdd023b5c87562d5e371b3d0ac0f4a7354d90a
Novandra Wichda Farun <vandrasurabaya@gmail.com>
2025-09-23 13:49:15 +0700

    Update Izin origin website

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ connectDB();
 
 const allowedOrigins = [
   "http://localhost:3000", //! untuk dev
-  "https://ukm-cyber-unusa.netlify.app", //? untuk deploy
+  "https://cyber-unusa.github.io", //? untuk deploy
 ];
 
 app.use(express.json());
